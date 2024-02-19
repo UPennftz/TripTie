49dda05bcc24bb233dbdfdaf42efc27002afaf82
Miki-design <73119146+Miklos-design@users.noreply.github.com>
2024-02-19 23:22:25 +0000

    added extra files, configurations, git ignore files, web pages, teplates, urls for both - the project and the app

---
+++ triptie/tripapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'tripapp/index.html')
+
+def about(request):
+    return render(request, 'tripapp/about.html')
+
+def login(request):
+    return render(request, 'tripapp/login.html')
+
+def profile(request):
+    return render(request, 'tripapp/profile.html')
+
+def myposts(request):
+    return render(request, 'tripapp/myposts.html')
+
+def messages(request):
+    return render(request, 'tripapp/messages.html')
+
+def explore(request):
+    return render(request, 'tripapp/explore.html')
--- triptie/triptie/settings.py
+++ triptie/triptie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@zl+9ggl!1dp)v^^_dqm*8&puh00#d_nov&et2bzp+_=l5ty*z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Allow any host for development purposes
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tripapp',  # Add your app here
 ]
 
 MIDDLEWARE = [
@@ -109,13 +110,20 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Add your static files directory here
+
+# Media files (User-uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # Add your media files directory here
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
