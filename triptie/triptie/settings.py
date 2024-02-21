47e2d803df277277e313d32110d61949d5996b57
Miki-design <73119146+Miklos-design@users.noreply.github.com>
2024-02-21 18:12:39 +0000

    added styling and css to the landing page, amended js and invoking of external media, did minor changes in the structure of the project

--- triptie/triptie/settings.py
+++ triptie/triptie/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,11 +108,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'  # This is where collectstatic will gather all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # This is where collectstatic will gather all static files
 
 # Media files (User-uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'  # Add your media files directory here
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'your_app/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
