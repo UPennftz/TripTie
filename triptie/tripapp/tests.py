3732ae68434d81e0927e0c9b3c74beb1e58612d4
Tianze Fei <2933521f@student.gla.ac.uk>
2024-02-23 17:43:40 +0000

    update

--- triptie/tripapp/tests.py
+++ triptie/tripapp/tests.py
@@ -3,6 +3,6 @@ from django.urls import reverse
 
 class YourAppTests(TestCase):
     def test_search_youtube(self):
-        response = self.client.get(reverse('your_view_name'), {'city': 'New York'})
+        response = self.client.get(reverse('your_view_name'), {'city': 'New York'}) # ???
         self.assertEqual(response.status_code, 200)
 
