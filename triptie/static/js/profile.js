fc5cf2dcae0b10e65492722fff90e82a49530e3b
zhifeng cheng <2876892C@student.gla.ac.uk>
2024-03-12 16:19:15 +0000

    update profile page

---
+++ triptie/static/js/profile.js
@@ -0,0 +1,15 @@
+function logout() {
+    // Implement the logout functionality
+}
+
+function editProfile() {
+    // Implement the edit profile functionality
+}
+
+function viewCollection() {
+    // Implement the view collection functionality
+}
+
+function viewPosts() {
+    // Implement the view posts functionality
+}
